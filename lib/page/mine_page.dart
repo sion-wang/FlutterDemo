7dc7098153b359618d3f09ab711a60c5ceacdeab
sion <sion044927@gmail.com>
2021-03-30 18:02:20 +0800

    Applied mine page ui.

--- lib/page/mine_page.dart
+++ lib/page/mine_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_demo/bloc/mine/mine_bloc.dart';
 import 'package:flutter_demo/bloc/mine/mine_event.dart';
 import 'package:flutter_demo/bloc/mine/mine_state.dart';
+import 'package:flutter_demo/model/api/bean/me/me.dart';
 
 class MinePage extends StatefulWidget {
   @override
@@ -24,14 +25,33 @@ class _MinePageState extends State<MinePage> {
   Widget build(BuildContext context) {
     return BlocBuilder<MineBloc, MineState>(builder: (context, state) {
       if (state is MineLoadedState) {
-        return Center(child: ClipOval(
-          child: Image.network(state.me.avatarUrl),
-        ),);
+        return _buildMe(state.me);
       } else if (state is MineFetchFailState) {
-        return Center(child: Text(state.error.toString()),);
+        return Center(
+          child: Text(state.error.toString()),
+        );
       } else {
-        return Center(child: CircularProgressIndicator(),);
+        return Center(
+          child: CircularProgressIndicator(),
+        );
       }
     });
+  }
+
+  Widget _buildMe(Me me) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1)),
+          ClipOval(child: Image.network(me.avatarUrl, width: 100, height: 100,),),
+          Padding(padding: EdgeInsets.only(top: 15)),
+          Text(me.login, style: TextStyle(fontSize: 20),),
+          Text(me.name, style: TextStyle(color: Colors.black45),),
+        ],
+      ),
+    );
   }
 }
